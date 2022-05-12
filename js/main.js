a08141bae90029447067cd620846d7fff88c0e52
BeainaT <tania.df@hotmail.it>
2022-05-12 18:22:51 +0200

    fixed timer + print on dom (basis)

--- js/main.js
+++ js/main.js
@@ -3,17 +3,21 @@
 let randomNumbers = [];
 // Genero per 5 volte un numero casuale
 for (let i = 0; i < 5; i++) {
-    let number = Math.floor(Math.random() * 10) + 1;
-    randomNumbers.push(number);
+    let number = Math.floor(Math.random() * 90) + 1;        
+        randomNumbers.push(number);
 }
+
 console.log(randomNumbers);
 // stampo array in pagina
 let body = document.querySelector("#container");
 body.append(randomNumbers);
-
+let result = document.createElement("div");
+body.append(result);
+let score = document.createElement("div");
+body.append(score);
 // Da lì parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-let timer = 5000;
+let timer = 30000;
 let userArray = [];
 setTimeout(function() {
     //eseguo 5 volte ciclo per chiedere all'utente di inserire un numero
@@ -23,14 +27,14 @@ setTimeout(function() {
         // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
         //SE tra i numeri generati casualmente è presente il numero inserito dall'utente
         if (randomNumbers.includes(userNumber)) {
-            //stampo il numero dell'utente
-            console.log("questi sono uguali", userNumber);
             //inserisco il suo numero in un array
             userArray.push(userNumber);
+            //stampo il numero dell'utente
+            result.innerHTML = ("hai indovinato questi numeri" + " " + userArray);
         }
-        //stampo la quantità di numeri inseriti, cioè i numeri indovinati
-        console.log("questa è la quantità", userArray.length);
     }
+    //stampo la quantità di numeri inseriti, cioè i numeri indovinati
+    score.innerHTML += ("ne hai indovinati" + " " + userArray.length);
 }, timer);
 
 
